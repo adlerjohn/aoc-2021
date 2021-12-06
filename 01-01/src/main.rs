0b54b582b62e5864bdb712e32c90507c715b5e24
John Adler <john.adler@protonmail.ch>
2021-12-05 19:29:39 -0500

    Solution.

--- 01-01/src/main.rs
+++ 01-01/src/main.rs
@@ -1,3 +1,23 @@
+use clap::{App, Arg};
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("aoc-01-01")
+        .arg(Arg::with_name("input").short("i").takes_value(true))
+        .get_matches();
+    let input = matches.value_of("input").unwrap_or("input");
+    let data = fs::read_to_string(input).expect("unable to read file");
+    let lines = data.split_whitespace().collect::<Vec<&str>>();
+
+    let mut count = 0;
+    let mut last = lines[0].parse::<u64>().unwrap();
+    for line in lines {
+        let val = line.parse::<u64>().unwrap();
+        if val > last {
+            count = count + 1;
+        }
+        last = val;
+    }
+
+    println!("{}", count);
 }
