24c95f405882b9dc544228875ba6122b478f3120
John Adler <john.adler@protonmail.ch>
2021-12-05 20:22:20 -0500

    Import.

---
+++ 03-01/src/main.rs
@@ -0,0 +1,31 @@
+use clap::{App, Arg};
+use std::fs;
+
+fn main() {
+    let matches = App::new("aoc-03-01")
+        .arg(Arg::with_name("input").short("i").takes_value(true))
+        .get_matches();
+    let input = matches.value_of("input").unwrap_or("input");
+    let data = fs::read_to_string(input).expect("unable to read file");
+    let lines = data.split("\n").collect::<Vec<&str>>();
+
+    let mut position = 0u64;
+    let mut depth = 0u64;
+    let mut aim = 0u64;
+    for line in lines {
+        let l = line.split_whitespace().collect::<Vec<&str>>();
+        let val = l[1].parse::<u64>().unwrap();
+        match l[0] {
+            "forward" => {
+                position += val;
+                depth += aim * val;
+            }
+            "down" => aim += val,
+            "up" => aim -= val,
+            _ => panic!("bad input"),
+        }
+        println!("{} {}", l[0], val);
+    }
+
+    println!("{}", position * depth);
+}
